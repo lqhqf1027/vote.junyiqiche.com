5a56c158620746c7438d59229fb90671def644bb
yinchuan.com <lqhqf422@gmail.com>
2019-01-11 13:00:00 +0800

    详情

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,7 +12,6 @@ use app\admin\model\Wechatuser;
 use app\common\model\Config as ConfigModel;
 use think\Db;
 use think\Session;
-use think\Config;
 
 class Index extends Frontend
 {
@@ -29,82 +28,19 @@ class Index extends Frontend
 
     public function index()
     {
+
         $contestant = $this->playerInfo(['status' => 'normal'], 'id,name,applicationimages,votes');
+
         $data = array_merge($this->publicData(), ['contestantList' => $contestant]);
 //        pr($data);
         $this->view->assign('data', $data);
         return $this->view->fetch();
     }
 
-
-    public function sharedata()
+    public function news()
     {
-        $url = input('urll');//获取当前页面的url，接收请求参数
-
-        $root['url'] = $url;
-        //获取access_token，并缓存
-        $file = RUNTIME_PATH . '/access_token';//缓存文件名access_token
-        $appid = Config::get('APPID'); // 填写自己的appid
-        $secret = Config::get('APPSECRET'); // 填写自己的appsecret
-        $expires = 3600;//缓存时间1个小时
-        if (file_exists($file)) {
-            $time = filemtime($file);
-            if (time() - $time > $expires) {
-                $token = null;
-            } else {
-                $token = file_get_contents($file);
-            }
-        } else {
-            fopen("$file", "w+");
-            $token = null;
-        }
-        if (!$token || strlen($token) < 6) {
-            $res = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . $appid . "&secret=" . $secret . "");
-
-            $res = json_decode($res, true);
-            $token = $res['access_token'];
-// write('access_token', $token, 3600);
-            @file_put_contents($file, $token);
-        }
-
-        //获取jsapi_ticket，并缓存
-        $file1 = RUNTIME_PATH . '/jsapi_ticket';
-        if (file_exists($file1)) {
-            $time = filemtime($file1);
-            if (time() - $time > $expires) {
-                $jsapi_ticket = null;
-            } else {
-                $jsapi_ticket = file_get_contents($file1);
-            }
-        } else {
-            fopen("$file1", "w+");
-            $jsapi_ticket = null;
-        }
-        if (!$jsapi_ticket || strlen($jsapi_ticket) < 6) {
-            $ur = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=$token&type=jsapi";
-            $res = file_get_contents($ur);
-            $res = json_decode($res, true);
-            $jsapi_ticket = $res['ticket'];
-            @file_put_contents($file1, $jsapi_ticket);
-        }
-
-        $timestamp = time();//生成签名的时间戳
-        $metas = range(0, 9);
-        $metas = array_merge($metas, range('A', 'Z'));
-        $metas = array_merge($metas, range('a', 'z'));
-        $nonceStr = '';
-        for ($i = 0; $i < 16; $i++) {
-            $nonceStr .= $metas[rand(0, count($metas) - 1)];//生成签名的随机串
-        }
-
-        $string1 = "jsapi_ticket=" . $jsapi_ticket . "&noncestr=" . $nonceStr . "&timestamp=" . $timestamp . "&url=" . $url . "";
-        $signature = sha1($string1);
-        $root['appid'] = $appid;
-        $root['nonceStr'] = $nonceStr;
-        $root['timestamp'] = $timestamp;
-        $root['signature'] = $signature;
-
-        return json_encode($root);
+        $newslist = [];
+        return jsonp(['newslist' => $newslist, 'new' => count($newslist), 'url' => 'https://www.fastadmin.net?ref=news']);
     }
 
     /**
@@ -149,6 +85,7 @@ class Index extends Frontend
             ->select())->toArray();
 
         $data = array_merge($this->publicData(), ['rankList' => $ranking]);
+
         $this->view->assign('data', $data);
         return $this->view->fetch();
     }
@@ -165,6 +102,7 @@ class Index extends Frontend
         $vote_rules = ConfigModel::get(['name' => 'vote_rules'])->value;
 
         $data = array_merge($this->publicData(), ['vote_rules' => $vote_rules]);
+
         $this->view->assign('data', $data);
         return $this->view->fetch();
     }
