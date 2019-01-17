4803d46da6753501ef543e1236d6d6e9be96ed7d
Cyruszx <17360268104@139.com>
2019-01-17 10:18:29 +0800

    改

--- application/extra/site.php
+++ application/extra/site.php
@@ -43,6 +43,6 @@ return array (
   'vote' => '2019-01-14 00:00:00',
   'vote_end' => '2019-01-25 23:59:59',
   'vote_rules' => '<div><img src="https://static.yc.junyiqiche.com/yinchuan/uploads/20190116/c2d205d293f16c492c3c7253f80b97ff.png" style="width: 689px;" data-filename="filename"><br></div><p></p>',
-  'link' => 'https://m10.music.126.net/20190116144108/ddfb656a37a983e2605e6af08c810076/ymusic/8667/e098/1c44/52fae5790c731c41491bb252958e4ed7.mp3',
+  'link' => 'http://other.web.ra01.sycdn.kuwo.cn/resource/n3/128/95/48/582978519.mp3',
   'switch' => '1',
 );
